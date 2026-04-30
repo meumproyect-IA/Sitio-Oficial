9d2742a1170f2b268d8ef5288c3aa0939493ada6
Germán Ruz <meumproyect@gmail.com>
2026-04-30 05:15:14 -0600

    MeumProyect IA 2026

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function actualizarFecha() {
+    const contenedor = document.getElementById('fecha-banner');
+    if (!contenedor) return;
+    const fecha = new Date();
+    const opciones = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    let texto = fecha.toLocaleDateString('es-ES', opciones);
+    contenedor.innerHTML = `Actualizado el: ${texto.charAt(0).toUpperCase() + texto.slice(1)}`;
+}
+document.addEventListener('DOMContentLoaded', actualizarFecha);
